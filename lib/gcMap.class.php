b9a33737f531430d5ea310723736894e17e24c7e
root <root@gw-iol>
2019-03-19 18:25:29 +0100

    fix filtro dinamico e filtro mapset

--- lib/gcMap.class.php
+++ lib/gcMap.class.php
@@ -391,68 +391,42 @@ class GCMap{
 				$oLayer=$oMap->getLayer($aLayersIndexes[$i]);
 				//Setto al volo il labelitem in altra lingua
 				if($langLabelitem = (isset($_SESSION[$myMap]["LABELITEM"][$idx][$oLayer->name][$langId]))?$_SESSION[$myMap]["LABELITEM"][$idx][$oLayer->name][$langId]:null) $oLayer->set('labelitem',$langLabelitem);
-				
 				//Setto al volo il filtro mapset
+				
 				if($_SESSION[$myMap]["FILTER"] && in_array($oLayer->name, $_SESSION[$myMap]["FILTER_LAYER"][$idx])){
 					$layerFilter = $_SESSION[$myMap]["FILTER"];
 					if($this->msVersion=='7'){
-						//TODO DA FINIRE ORA PRENDE SOLO UN FILTRO
-						$processing=$oLayer->getProcessing(); ///.......TODO
-						print_debug($processing,null,'processing');
-						for($i=0;$i<count($processing);$i++){
-							if(strpos("NATIVE_FILTER",$processing[$i])!==false){
-								$layerFilter = $processing[$i];
-							}
+						//ATTENZIONE per velocizzare considero che processing sia fatto da 2 voci CLOSE_CONNECTION e NATIVE_FILTER
+						$processing=$oLayer->getProcessing();
+						if(count($processing)>1){
+							$layerFilter = str_replace("\"","",$processing[1])." AND " .$layerFilter;
 						}
-						$oLayer->setprocessing("NATIVE_FILTER=" . $layerFilter);
+						$oLayer->clearProcessing();
+						$oLayer->setprocessing("CLOSE_CONNECTION=DEFER");
+						$oLayer->setprocessing($layerFilter);
 					}
 					else{
 						if($oLayer->getFilterString()) $layerFilter = str_replace("\"","",$oLayer->getFilterString())." AND " .$layerFilter;
 						$oLayer->setFilter($layerFilter);
 					}
 				} 
-			/*
-			
-				if($_SESSION[$myMap]["FILTER"] && in_array($oLayer->name, $_SESSION[$myMap]["FILTER_LAYER"][$idx])){
-					$layerFilter = $_SESSION[$myMap]["FILTER"];
-					if($this->msVersion=='8'){
-						$processing=$oLayer->getProcessing(); 
-						$indexFilter = null;
-						for($i=0;$i<count($processing);$i++){
-							print_debug(strpos($processing[$i],"NATIVE_FILTER"),null,'processing2');
-							if(strpos($processing[$i],"NATIVE_FILTER")==0){
-								$indexFilter = $i;
-							}
-						}
-						if ($indexFilter != null){
-							$processing[$indexFilter] = str_replace("\"","",$processing[$indexFilter])." AND " .$layerFilter;
-						}
-						//$oLayer->clearProcessing();
-						//for($i=0;$i<count($processing);$i++){
-						//	$oLayer->setprocessing($processing[$i]);
-						//}
-					}
-					else{
-						$currentFilter = $oLayer->getFilterString();
-						if ($currentFilter!=''){
-							$layerFilter = str_replace("\"","",$currentFilter)." AND " .$layerFilter;
-						}						
-						$oLayer->setFilter($layerFilter);
-					}
-				} 
-			
-			
-			
-			*/
-			
-			
-			
-			
+
 				$dynamicFilter = $this->getDynamicFilter($oLayer->name);
-				//print_debug($dynamicFilter,null,'filter');
 				if($dynamicFilter){//layer dinamico e oggetti da restituire in mappa
-					if($oLayer->getFilterString()) $dynamicFilter = str_replace("\"","",$oLayer->getFilterString())." AND " .$dynamicFilter;
-					$oLayer->setFilter($dynamicFilter);
+					if($this->msVersion=='7'){
+						//ATTENZIONE per velocizzare considero che processing sia fatto da 2 voci CLOSE_CONNECTION e NATIVE_FILTER
+						$processing=$oLayer->getProcessing();
+						if(count($processing)>1){
+							$dynamicFilter = str_replace("\"","",$processing[1])." AND " .$dynamicFilter;
+						}
+						$oLayer->clearProcessing();
+						$oLayer->setprocessing("CLOSE_CONNECTION=DEFER");
+						$oLayer->setprocessing($dynamicFilter);					
+					}
+					else{					
+						if($oLayer->getFilterString()) $dynamicFilter = str_replace("\"","",$oLayer->getFilterString())." AND " .$dynamicFilter;
+						$oLayer->setFilter($dynamicFilter);
+					}
 					$oLayer->set('status',$status);//ci sono oggetti da restituire accedo il layer
 				} 
 				else{
